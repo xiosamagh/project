71f6de41e3bf44cb6ef048c5c2e261b753844523
Ilya <example@gmail.com>
2022-12-06 16:03:37 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer);
+
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваше Фамилия?', '');
+answers[2] = prompt('Как ваше Возраст?', '');
+
+
+document.write(answers);
